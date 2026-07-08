9f6f503c381bc674a5fd7f45cf59ac5fdeb15309
RanierDalton <ranier.couto@sptech.school>
2026-07-08 13:40:55 -0300

    chore: update prerender to use relative asset paths

--- prerender.js
+++ prerender.js
@@ -5,13 +5,17 @@ import server from './dist/server/server.js';
 async function main() {
   const req = new Request("http://localhost/");
   const res = await server.fetch(req, {}, {});
-  const html = await res.text();
+  let html = await res.text();
+  
+  // Replace absolute /assets/ and /favicon.ico with relative ./assets/ and ./favicon.ico
+  html = html.replace(/(href|src)="\/assets\//g, '$1="./assets/');
+  html = html.replace(/(href|src)="\/favicon\.ico"/g, '$1="./favicon.ico"');
   
   const outputDir = path.resolve('dist/client');
   fs.writeFileSync(path.join(outputDir, 'index.html'), html);
   fs.writeFileSync(path.join(outputDir, '404.html'), html);
-  fs.writeFileSync(path.join(outputDir, 'CNAME'), "ranierdalton.is-a.dev\n");
-  console.log("Prerendered index.html, 404.html and CNAME successfully!");
+  // fs.writeFileSync(path.join(outputDir, 'CNAME'), "ranierdalton.is-a.dev\n");
+  console.log("Prerendered index.html and 404.html with relative paths successfully!");
 }
 
 main().catch(console.error);
